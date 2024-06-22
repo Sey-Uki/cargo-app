fa18a97570f3836535f785ec6b4ab004b0b17407
Sey-Uki <abakarova.diana99@gmail.com>
2024-06-22 16:43:14 +0300

    change tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,10 @@
 import { Tabs } from "expo-router";
 import React from "react";
 
-import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -18,11 +19,28 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          title: "Заказы",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "home" : "home-outline"}
+            <MaterialCommunityIcons
+              name={focused ? "truck-delivery" : "truck-delivery-outline"}
+              size={24}
               color={color}
+              style={{ marginBottom: -3 }}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Профиль",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "person-circle" : "person-circle-outline"}
+              size={24}
+              color={color}
+              style={{ marginBottom: -3 }}
             />
           ),
         }}
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "@gluestack-ui/themed";
+export default function profile() {
+  return (
+    <View>
+      <View style={{ height: 100 }} />
+      <Text>123</Text>
+    </View>
+  );
+}
