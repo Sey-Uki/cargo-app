e7808a44f74b9b79c868a2f25ef832809a44644b
Sey-Uki <abakarova.diana99@gmail.com>
2024-06-23 17:09:37 +0300

    change profile style

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,55 @@
-import { Text, View } from "@gluestack-ui/themed";
+import { ArrowRight } from "@/components/ArrowRight";
+import { TopBar } from "@/components/TopBar";
+import { Button, ButtonText, Text, View } from "@gluestack-ui/themed";
+
 export default function profile() {
   return (
-    <View>
-      <View style={{ height: 100 }} />
-      <Text>123</Text>
+    <View flex={1} justifyContent="space-between">
+      <View>
+        <View style={{ height: 70 }} />
+        <TopBar text="Профиль" />
+        <View alignItems="center" padding={20}>
+          <View
+            width={64}
+            height={64}
+            style={{ backgroundColor: "#828282" }}
+            borderRadius={100}
+          />
+        </View>
+        <View>
+          <View
+            flexDirection="row"
+            justifyContent="space-between"
+            height={48}
+            alignItems="center"
+            padding={14}
+          >
+            <Text fontWeight={500}>Имя</Text>
+            <Text>Ахмад Ахмадов</Text>
+            <ArrowRight />
+          </View>
+          <View
+            flexDirection="row"
+            justifyContent="space-between"
+            height={48}
+            alignItems="center"
+            padding={14}
+          >
+            <Text fontWeight={500}>Email</Text>
+            <Text>name@domain.com</Text>
+            <ArrowRight />
+          </View>
+        </View>
+      </View>
+      <Button
+        margin={14}
+        marginBottom={21}
+        size="lg"
+        borderRadius={8}
+        bgColor="#CE1D1D"
+      >
+        <ButtonText>Выйти</ButtonText>
+      </Button>
     </View>
   );
 }
