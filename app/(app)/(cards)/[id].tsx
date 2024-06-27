c8a923fb3fe1c154614d64fa2762e0dc88881a01
Sey-Uki <abakarova.diana99@gmail.com>
2024-06-27 22:00:11 +0300

    change page id cards

--- app/(app)/(cards)/[id].tsx
+++ app/(app)/(cards)/[id].tsx
@@ -35,47 +35,53 @@ export default function Info() {
         text="Информация"
       />
 
-      <View margin={15}>
-        <Heading size="xl">Заказ #{order.id}</Heading>
-        <Image
-          source={order.img}
-          style={{ width: "100%", height: 150 }}
-          marginBottom={17}
-          marginTop={27}
-          alt="Груз"
-        />
-        <View gap={11}>
-          <View flexDirection="row" justifyContent="space-between">
-            <Text color="$black">Вес</Text>
-            <Text color="$black">{order.weight}</Text>
+      <View margin={15} flex={1} justifyContent="space-between">
+        <View>
+          <Heading size="xl">Заказ #{order.id}</Heading>
+          <Image
+            source={order.img}
+            style={{ width: "100%", height: 150 }}
+            marginBottom={17}
+            marginTop={27}
+            alt="Груз"
+          />
+          <View gap={11}>
+            <View flexDirection="row" justifyContent="space-between">
+              <Text color="$black">Вес</Text>
+              <Text color="$black">{order.weight}</Text>
+            </View>
+            <View flexDirection="row" justifyContent="space-between">
+              <Text color="$black">Объем</Text>
+              <Text color="$black">{order.volume}</Text>
+            </View>
+            <View flexDirection="row" justifyContent="space-between">
+              <Text color="$black">Трекинг</Text>
+              <Text color="$black">{order.location}</Text>
+            </View>
+            <View flexDirection="row" justifyContent="space-between">
+              <Text color="$black">Статус</Text>
+              <Text color="$black">
+                {order.status === "paid" ? "Оплачено" : "Ждет оплаты"}
+              </Text>
+            </View>
           </View>
-          <View flexDirection="row" justifyContent="space-between">
-            <Text color="$black">Объем</Text>
-            <Text color="$black">{order.volume}</Text>
-          </View>
-          <View flexDirection="row" justifyContent="space-between">
-            <Text color="$black">Трекинг</Text>
-            <Text color="$black">{order.location}</Text>
-          </View>
-          <View flexDirection="row" justifyContent="space-between">
-            <Text color="$black">Статус</Text>
-            <Text color="$black">
-              {order.status === "paid" ? "Оплачено" : "Ждет оплаты"}
+          <Text underline marginTop={15} color="#81838F">
+            Накладная
+          </Text>
+          <View
+            marginTop={36}
+            flexDirection="row"
+            justifyContent="space-between"
+          >
+            <Text size="xl" fontWeight="$medium" color="$black">
+              Итого
+            </Text>
+            <Text size="xl" fontWeight="$medium" color="$black">
+              {order.cost}
             </Text>
           </View>
         </View>
-        <Text underline marginTop={15} color="#81838F">
-          Накладная
-        </Text>
-        <View marginTop={36} flexDirection="row" justifyContent="space-between">
-          <Text size="xl" fontWeight="$medium" color="$black">
-            Итого
-          </Text>
-          <Text size="xl" fontWeight="$medium" color="$black">
-            {order.cost}
-          </Text>
-        </View>
-        <FormControl gap={5} marginTop={15}>
+        <FormControl gap={5} marginTop={15} paddingBottom={40}>
           <Button bg="$black" borderRadius="$md" height={52}>
             <ButtonText fontSize="$sm" fontWeight="$medium">
               Отслеживание
