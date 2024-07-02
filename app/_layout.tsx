1aed2bca43f74a98b5b8e92ba876dcb3710c38cf
Sey-Uki <abakarova.diana99@gmail.com>
2024-07-03 01:12:07 +0300

    add redux-persist

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,16 @@
-import { store } from "@/store";
+import { persistor, store } from "@/store";
 import { Stack } from "expo-router";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function Root() {
   return (
     <Provider store={store}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(app)" />
-      </Stack>
+      <PersistGate loading={null} persistor={persistor}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(app)" />
+        </Stack>
+      </PersistGate>
     </Provider>
   );
 }
--- store/index.ts
+++ store/index.ts
@@ -1,15 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { useSelector, TypedUseSelectorHook, useDispatch } from 'react-redux';
 import auth from './slices/auth';
 import user from './slices/user';
 import orders from './slices/orders';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: [
+    'user',
+    'auth',
+  ]
+}
+
+export const rootReducer = combineReducers({
+  auth,
+  user,
+  orders
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    auth,
-    user,
-    orders
-  },
+  reducer: persistedReducer
 });
 
 export type RootState = ReturnType<typeof store.getState>;
@@ -17,3 +32,5 @@ export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch: () => AppDispatch = useDispatch;
+
+export const persistor = persistStore(store)
