f3a5b7d49fb1256b1c70d4eb7f04b1fb30bf9a9a
Sey-Uki <abakarova.diana99@gmail.com>
2024-08-27 14:37:55 +0300

    load medium and reqular fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,28 @@ import { Stack } from "expo-router";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import {useEffect} from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
 export default function Root() {
+  const [loaded, error] = useFonts({
+    'SFProText-Regular': require('../assets/fonts/SFProText-Regular.ttf'),
+    'SFProText-Medium': require('../assets/fonts/SFProText-Medium.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
