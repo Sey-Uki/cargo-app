0dbcb1fa0d220a2f39a127455928be294e740707
Sey-Uki <abakarova.diana99@gmail.com>
2024-08-29 01:07:29 +0300

    add redux-persist-expire

--- store/index.ts
+++ store/index.ts
@@ -1,30 +1,39 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { useSelector, TypedUseSelectorHook, useDispatch } from 'react-redux';
-import auth from './slices/auth';
-import user from './slices/user';
-import orders from './slices/orders';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { persistStore, persistReducer } from 'redux-persist';
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { useSelector, TypedUseSelectorHook, useDispatch } from "react-redux";
+import auth from "./slices/auth";
+import user from "./slices/user";
+import orders from "./slices/orders";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistStore, persistReducer } from "redux-persist";
+import expireReducer from "redux-persist-expire";
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage: AsyncStorage,
-  whitelist: [
-    'user',
-    'auth',
-  ]
-}
+  whitelist: ["user", "auth"],
+  transforms: [
+    expireReducer("auth", {
+      expireSeconds: 86400,
+      autoExpire: true,
+    }),
+
+    expireReducer("user", {
+      expireSeconds: 86400,
+      autoExpire: true,
+    }),
+  ],
+};
 
 export const rootReducer = combineReducers({
   auth,
   user,
-  orders
+  orders,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer
+  reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
@@ -33,4 +42,4 @@ export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch: () => AppDispatch = useDispatch;
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
