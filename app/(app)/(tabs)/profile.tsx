67dd8988aa1e31076070a2df398d9a6d253ac168
Sey-Uki <abakarova.diana99@gmail.com>
2024-08-29 23:30:09 +0300

    change file structure and backgroundcolor

--- app/(app)/(tabs)/profile.tsx
+++ app/(app)/(tabs)/profile.tsx
@@ -11,57 +11,64 @@ export default function Profile() {
   const dispatch = useAppDispatch();
 
   return (
-    <View flex={1} justifyContent="space-between" backgroundColor="#fff">
-      <View>
-        <View style={{ height: 70 }} />
-        <TopBar text="Профиль" />
-        <View alignItems="center" padding={20}>
-          <View
-            width={64}
-            height={64}
-            style={{ backgroundColor: "#828282" }}
-            borderRadius={100}
-          />
-        </View>
+    <View flex={1} backgroundColor="#fff">
+      <View style={{ height: 70 }} />
+      <TopBar text="Профиль" />
+      <View justifyContent="space-between" backgroundColor="#F2F2F7" flex={1}>
         <View>
-          <View
-            flexDirection="row"
-            justifyContent="space-between"
-            height={48}
-            alignItems="center"
-            padding={14}
-          >
-            <Text color="$black" fontWeight={500}>Имя</Text>
-            <Text color="$black">{userData?.firstName} {userData?.lastName}</Text>
-            <ArrowRight />
+          <View alignItems="center" padding={20}>
+            <View
+              width={64}
+              height={64}
+              style={{ backgroundColor: "#828282" }}
+              borderRadius={100}
+            />
           </View>
-          <View
-            flexDirection="row"
-            justifyContent="space-between"
-            height={48}
-            alignItems="center"
-            padding={14}
-          >
-            <Text color="$black" fontWeight={500}>Email</Text>
-            <Text color="$black">{userData?.email}</Text>
-            <ArrowRight />
+          <View>
+            <View
+              flexDirection="row"
+              justifyContent="space-between"
+              height={48}
+              alignItems="center"
+              padding={14}
+            >
+              <Text color="$black" fontWeight={500}>
+                Имя
+              </Text>
+              <Text color="$black">
+                {userData?.firstName} {userData?.lastName}
+              </Text>
+              <ArrowRight />
+            </View>
+            <View
+              flexDirection="row"
+              justifyContent="space-between"
+              height={48}
+              alignItems="center"
+              padding={14}
+            >
+              <Text color="$black" fontWeight={500}>
+                Email
+              </Text>
+              <Text color="$black">{userData?.email}</Text>
+              <ArrowRight />
+            </View>
           </View>
         </View>
+        <Button
+          margin={14}
+          marginBottom={21}
+          size="lg"
+          borderRadius={8}
+          bgColor="#CE1D1D"
+          onPress={() => {
+            dispatch(logOut());
+            router.replace("/sign_in");
+          }}
+        >
+          <ButtonText>Выйти</ButtonText>
+        </Button>
       </View>
-      <Button
-        margin={14}
-        marginBottom={21}
-        size="lg"
-        borderRadius={8}
-        bgColor="#CE1D1D"
-        onPress={() => {
-          dispatch(logOut());
-
-          router.replace('/sign_in');
-        }}
-      >
-        <ButtonText>Выйти</ButtonText>
-      </Button>
     </View>
   );
 }
