00fd07e0fc54d1cfa51c13144b37eb8bc4cdacb8
Sey-Uki <abakarova.diana99@gmail.com>
2024-08-30 00:36:18 +0300

    created a component for email and phone

---
+++ components/EmailAndPhone.tsx
@@ -0,0 +1,29 @@
+import { Text, View } from "@gluestack-ui/themed";
+
+export const EmailAndPhone = ({ email, phone }: any) => {
+  return (
+    <View backgroundColor="#fff" borderRadius={10}>
+      <View
+        padding={16}
+        flexDirection="row"
+        borderBottomColor="#E6E6E6"
+        borderBottomWidth={1}
+      >
+        <Text color="$black" fontWeight={500} width={100}>
+          Email
+        </Text>
+        <Text color="#3C3C434D" fontFamily="SFProText-Regular" fontSize={17}>
+          {email}
+        </Text>
+      </View>
+      <View padding={16} flexDirection="row">
+        <Text color="$black" fontWeight={500} width={100}>
+          Телефон
+        </Text>
+        <Text color="#3C3C434D" fontFamily="SFProText-Regular" fontSize={17}>
+          {phone}
+        </Text>
+      </View>
+    </View>
+  );
+};
