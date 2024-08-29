aaafeb98cc60772cadf7c07a0c1168805d109693
Sey-Uki <abakarova.diana99@gmail.com>
2024-08-29 23:57:05 +0300

    created a user info component

--- app/(app)/(tabs)/profile.tsx
+++ app/(app)/(tabs)/profile.tsx
@@ -1,9 +1,9 @@
-import { ArrowRight } from "@/components/ArrowRight";
+import { ProfileText } from "@/components/ProfileText";
 import { TopBar } from "@/components/TopBar";
 import { useAppDispatch, useAppSelector } from "@/store";
 import { logOut } from "@/store/slices/auth";
 import { selectUserData } from "@/store/slices/user";
-import { Button, ButtonText, Text, View } from "@gluestack-ui/themed";
+import { Button, ButtonText, View } from "@gluestack-ui/themed";
 import { router } from "expo-router";
 
 export default function Profile() {
@@ -14,49 +14,26 @@ export default function Profile() {
     <View flex={1} backgroundColor="#fff">
       <View style={{ height: 70 }} />
       <TopBar text="Профиль" />
-      <View justifyContent="space-between" backgroundColor="#F2F2F7" flex={1}>
+      <View
+        justifyContent="space-between"
+        backgroundColor="#F2F2F7"
+        flex={1}
+        paddingHorizontal={16}
+      >
         <View>
-          <View alignItems="center" padding={20}>
-            <View
-              width={64}
-              height={64}
-              style={{ backgroundColor: "#828282" }}
-              borderRadius={100}
-            />
-          </View>
-          <View>
-            <View
-              flexDirection="row"
-              justifyContent="space-between"
-              height={48}
-              alignItems="center"
-              padding={14}
-            >
-              <Text color="$black" fontWeight={500}>
-                Имя
-              </Text>
-              <Text color="$black">
-                {userData?.firstName} {userData?.lastName}
-              </Text>
-              <ArrowRight />
-            </View>
-            <View
-              flexDirection="row"
-              justifyContent="space-between"
-              height={48}
-              alignItems="center"
-              padding={14}
-            >
-              <Text color="$black" fontWeight={500}>
-                Email
-              </Text>
-              <Text color="$black">{userData?.email}</Text>
-              <ArrowRight />
-            </View>
-          </View>
+          <ProfileText
+            infoText={"Имя"}
+            userText={`${userData?.firstName} ${userData?.lastName}`}
+          />
+
+          <ProfileText infoText={"Email"} userText={userData?.email} />
+
+          <ProfileText infoText={"Телефон"} userText={"+7 (999) 999 99-99"} />
+
+          <ProfileText infoText={"Код"} userText={"6438819"} />
         </View>
+
         <Button
-          margin={14}
           marginBottom={21}
           size="lg"
           borderRadius={8}
