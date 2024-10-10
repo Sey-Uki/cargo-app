91cb045f010f03be87a4d250828ec400ec4a7b9f
Sey-Uki <abakarova.diana99@gmail.com>
2024-10-10 15:56:11 +0300

    ref

--- components/ImageList.tsx
+++ components/ImageList.tsx
@@ -19,6 +19,7 @@ export const ImageList = ({ images }: Props) => {
         return (
           <Image
             size="md"
+            key={item.title}
             source={{
               uri: item.src,
             }}
